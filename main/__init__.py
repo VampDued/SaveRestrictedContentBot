0964a0017d96c465b930fec08227b6d8dd7595df
VampDued <131732886+VampDued@users.noreply.github.com>
2023-12-31 10:23:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("1814890", default=None, cast=int)
+API_HASH = config("346db519688cd678a2bc5908d3791967", default=None)
+BOT_TOKEN = config("6559784740:AAHnMsnhL2n4OLa_md1UTLj2yZ7hYf1nSBU", default=None)
+SESSION = config("BQHBmd8AIGksJcgQx2fPuF2IUaHBYPnCtbO8RiX4L6UqZnBYzTd423YOKFa306XCbsOEN7Jy2VprHdJhjSYSWc9gUMRPe7tM0xtD2QL0viAw1xOZAS-KO1rZgrCTx8-pEJPNyGIFgjZNIi-7x72iIMvAVBupr4tgYHd2eJj8lgRiZAXNR55EWY0VXE-yNccb3Xcewbv2_JGz0d7AdJ-lVOnZOWaZUwzLNiudWiVWV7PyosYbwfy8dOZ7Q1f-62Fwc0fOwnf1aGj0xantiJS1Wt1SwxZg8IVYf6TlrcnlBfeOBuBM5u4t6m96dL3ScKE57cKSCbtKhivpiz4w_uU21qU_ySU-3gAAAABF1gTVAA", default=None)
+FORCESUB = config("TNTOONBOT", default=None)
+AUTH = config("1171653845", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
